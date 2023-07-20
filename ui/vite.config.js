31bbd5c8b961c4bc6806225ab31a36265b26a9e1
“huailei000” <2280131253@qq.com>
2023-07-20 19:15:30 +0800

    feat: 添加svg图标

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from "vite-plugin-windicss"
 import path from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 const resolve = (dir) => path.join(__dirname, dir)
 
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,16 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: '/kael/',
-    plugins: [vue(), WindiCSS()],
+    plugins: [
+      vue(),
+      WindiCSS(),
+      createSvgIconsPlugin({
+        // 指定要缓存的文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // 指定symbolId格式
+        symbolId: '[name]'
+      })
+    ],
     resolve: {
       alias: {
         '@': resolve('src')
